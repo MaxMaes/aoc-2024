10a6bccdc2f51ed5cf277eb653502987d964915d
Max Maes <max@livewallgroup.com>
2024-12-02 09:15:25 +0100

    feat: Initial setup

---
+++ src/main/kotlin/Utils.kt
@@ -0,0 +1,7 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+/**
+ * Reads lines from the given input txt file.
+ */
+fun readInput(name: String) = Path("src/inputs/$name.txt").readLines()
